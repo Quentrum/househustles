e5c4961b770aacb3a6ee1f587b1a5ec5ac6f5518
Veltson Bastien <43249253+veltsonbastien@users.noreply.github.com>
2019-05-26 01:16:17 -0700

    fixed the option select

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
 //HouseHustles 2019
 
-function scrollToMain(){
- var scrollTo = $("#scrollToMainLink").offset.top(); //get top of main page
- $('html, body').animate({
-          scrollTop: scrollTo
-        }, 1000
-      );
-}
+$(document).ready(function(){
+
+//OPTION BOXES
+var state = 0; //click state
+
+  //find me listings
+  $("#option1").click(function(){
+  }, function(){ state = 1;showLightBox(state);}); //change global state to 1
+
+  //house scout for me
+  $("#option2").click(function(){
+    showLightBox(); //open the lightbox
+  }, function(){ state = 2; showLightBox(state);}); //change global state to 2
+
+  function showLightBox(param){
+    
+  }
+
+
+}); //end of DOCUMENT READY
