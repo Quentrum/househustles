1cd35e36ea15675cc6eccae7af16d95f87f7274a
Veltson Bastien <43249253+veltsonbastien@users.noreply.github.com>
2019-05-26 14:58:35 -0700

    finished the option selects

--- js/app.js
+++ js/app.js
@@ -2,8 +2,10 @@
 
 $(document).ready(function(){
 
-//OPTION BOXES
-var state = 0; //click state
+  //OPTION BOXES
+  var state = 0; //click state
+
+  $("#optionLightBox1").find("div").css({"display":"none"});
 
   //find me listings
   $("#option1").click(function(){
@@ -14,9 +16,30 @@ var state = 0; //click state
     showLightBox(); //open the lightbox
   }, function(){ state = 2; showLightBox(state);}); //change global state to 2
 
-  function showLightBox(param){
-    
-  }
+  $("#cancelLink").click(function(){
+     $("#optionLightBox1").find("div").fadeOut(1000);
+     $("#optionLightBox1").addClass("fade-out");
+     $("#optionLightBox1").removeClass("fade-in");
+     $([document.documentElement, document.body]).animate({ scrollTop: $("#option1").offset().top}, 100);
+  });
 
+  $("#cancelLink2").click(function(){
+    $("#optionLightBox2").find("div").fadeOut(1000);
+    $("#optionLightBox2").addClass("fade-out");
+    $("#optionLightBox2").removeClass("fade-in");
+  });
+
+  function showLightBox(param){
+    if(param == 1) {
+      $("#optionLightBox1").removeClass("fade-out");
+      $("#optionLightBox1").find("div").fadeIn(1000, function(){ $("#form-name-email").css({"display":"inline-flex"});} );
+      $("#optionLightBox1").addClass("fade-in");
+      $([document.documentElement, document.body]).animate({ scrollTop: $("#optionLightBox1").offset().top-30}, 500);
+    }
+    else if(param == 2) {
+       $("#optionLightBox2").removeClass("fade-out");
+       $("#optionLightBox1").find("div").fadeIn(1000);
+       $("#optionLightBox2").addClass("fade-in"); }
+  }
 
 }); //end of DOCUMENT READY
